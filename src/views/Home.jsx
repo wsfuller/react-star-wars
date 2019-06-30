188c2f74115f8b0962fc6c2c008bc341ddf8ec12
wfuller <wfuller87@gmail.com>
2019-06-30 11:14:29 -0700

    Adding Films to homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import FilmsList from '../components/Films/ListContainer';
 
-const Home = () => <h1>This is the Homepage</h1>;
+const Home = () => {
+  const randomNumber = Math.floor(Math.random() * 10);
+
+  return (
+    <main>
+      This is the Homepage
+      <FilmsList
+        numberOfFilms={4}
+        filmsOrderBy={`${randomNumber % 2 === 0 ? 'episodeId_ASC' : 'episodeId_DESC'}`}
+        gridOptions={{
+          xs: 12, sm: 6, md: 3, lg: 3,
+        }}
+      />
+    </main>
+  );
+};
 
 export default Home;
