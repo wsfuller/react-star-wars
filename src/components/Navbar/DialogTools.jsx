612d905ff16d491c8ca391af7abfb114cf2f899c
wfuller <wfuller87@gmail.com>
2019-07-01 11:04:58 -0700

    Adding modal and drawer functionality

---
+++ src/components/Navbar/DialogTools.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DialogTools = () => (
+  <div>
+    <h1>React Star Wars Tools</h1>
+    <p>Some information in here</p>
+  </div>
+);
+
+export default DialogTools;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,132 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
+import ReactStarWarsLogo from '../../assets/images/logos/react-star-wars.svg';
+import MoreMenu from './MoreMenu';
+import DialogAbout from './DialogAbout';
+import DialogTools from './DialogTools';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawer: false,
+      dialog: false,
+      whichDialog: '',
+    };
+    this.openDialog = this.openDialog.bind(this);
+  }
+
+  toggleDrawer = () => {
+    const { drawer } = this.state;
+
+    this.setState({
+      drawer: !drawer,
+    });
+  };
+
+  openDialog = (selectedDialog) => {
+    this.setState({
+      dialog: true,
+      whichDialog: selectedDialog,
+    });
+  };
+
+  closeDialog = () => {
+    this.setState({
+      dialog: false,
+      whichDialog: '',
+    });
+  };
+
+  dialogToRender = () => {
+    const { whichDialog } = this.state;
+    let dialogContent;
+    if (whichDialog === 'about') {
+      dialogContent = <DialogAbout />;
+    } else if (whichDialog === 'tools') {
+      dialogContent = <DialogTools />;
+    } else {
+      dialogContent = <div />;
+    }
+    return dialogContent;
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { drawer, dialog } = this.state;
+
+    return (
+      <Fragment>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+              role="button"
+              onClick={() => this.toggleDrawer()}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              <Link className={classes.branding} to="/">
+                <img
+                  className={classes.imageResponsive}
+                  src={ReactStarWarsLogo}
+                  alt="Logo for React Star Wars"
+                />
+              </Link>
+            </Typography>
+            <MoreMenu openDialog={this.openDialog} />
+          </Toolbar>
+        </AppBar>
+        <Drawer open={drawer} onClose={() => this.toggleDrawer()}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={() => this.toggleDrawer()}
+            onKeyDown={() => this.toggleDrawer()}
+          >
+            Hi There
+          </div>
+        </Drawer>
+        <Dialog open={dialog} onClose={this.closeDialog} aria-labelledby="more-dialog-title">
+          {this.dialogToRender()}
+        </Dialog>
+      </Fragment>
+    );
+  }
+}
+
+NavBar.propTypes = {
+  classes: PropTypes.shape({}),
+};
+
+NavBar.defaultProps = {
+  classes: {},
+};
+
+const styles = theme => ({
+  branding: {
+    maxWidth: 150,
+    display: 'flex',
+  },
+  imageResponsive: {
+    maxWidth: '100%',
+    height: 'auto',
+    display: 'block',
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -18,26 +134,6 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
-}));
+});
 
-const NavBar = () => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            React Star Wars
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
-
-export default NavBar;
+export default withStyles(styles)(NavBar);
