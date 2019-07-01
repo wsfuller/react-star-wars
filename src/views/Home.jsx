8fa77f672507dd0da523f84fd8062fd0f369ae35
wfuller <wfuller87@gmail.com>
2019-06-30 22:23:36 -0700

    Updating home about section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,12 +4,17 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import Section from '../components/Section';
 import FilmsList from '../components/Films/ListContainer';
 import Hero from '../components/Hero';
 import HeroImage from '../assets/images/hero-backgrounds/tie-fighers-and-star-destroyer.jpg';
 import ReactStarWarsLogo from '../assets/images/logos/react-star-wars.svg';
+import ModernTools from '../assets/images/tools/comps/react-apollo-graphql.svg';
 
 const useStyles = makeStyles(theme => ({
   hero: {
@@ -35,6 +40,9 @@ const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
   },
+  cardsMedia: {
+    height: 145,
+  },
 }));
 
 const Home = () => {
@@ -54,14 +62,86 @@ const Home = () => {
               color="primary"
               className={classes.button}
               type="button"
-              href="https://swapi.co/"
+              href="https://graphiql.graphcms.com/simple/v1/swapi"
             >
-              Powered by SWAPI
+              Powered by SWAPI GraphQL
             </Button>
           </div>
 )}
         className={classes.hero}
       />
+      <Section>
+        <Container>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="h2" gutterBottom>
+                About
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  className={classes.cardsMedia}
+                  image={ModernTools}
+                  title="React + Apollo + GraphQL"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Modern Tooling
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    React Star Wars is a modern web application built with React and GraphQL
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" href="https://reactjs.org/">
+                    React
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    href="https://www.apollographql.com/docs/react/"
+                  >
+                    Apollo Client
+                  </Button>
+                  <Button size="small" color="primary" href="https://graphql.org/">
+                    GraphQL
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  className={classes.cardsMedia}
+                  image={ModernTools}
+                  title="React + Apollo + GraphQL"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Modern Tooling
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    React Star Wars is a modern web application built with React and GraphQL
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary" href="https://reactjs.org/">
+                    SWAPI
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    href="https://graphiql.graphcms.com/simple/v1/swapi"
+                  >
+                    SWAPI GraphQL
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </Section>
       <Section>
         <Container>
           <Grid container>
