1b1d7f2f2490b2769267809b72ffe60322a21f04
Steve Fuller <wfuller87@gmail.com>
2019-07-01 00:02:53 -0700

    Feature/footer (#3)
    * Creating Footer component
    
    * Adding Footer to all pages
    
    * Optimizing Footer WSF logo

--- src/components/Application/index.jsx
+++ src/components/Application/index.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Navbar from '../Navbar';
 import Routes from '../../views/Routes';
+import Footer from '../Footer';
 
 const App = () => (
   <Fragment>
     <Navbar />
     <Routes />
+    <Footer />
   </Fragment>
 );
 
