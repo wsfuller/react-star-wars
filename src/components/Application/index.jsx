48d42838238809106ec7c735b8a616cf9b319279
wfuller <wfuller87@gmail.com>
2019-07-01 20:57:36 -0700

    Implementing sticky footer concept

--- src/components/Application/index.jsx
+++ src/components/Application/index.jsx
@@ -1,14 +1,30 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
 import Navbar from '../Navbar';
 import Routes from '../../views/Routes';
 import Footer from '../Footer';
 
-const App = () => (
-  <Fragment>
-    <Navbar />
-    <Routes />
-    <Footer />
-  </Fragment>
-);
+const useStyles = makeStyles(() => ({
+  applicationWrapper: {
+    display: 'flex',
+    minHeight: '100vh',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.applicationWrapper}>
+      <Navbar />
+      <Routes />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
