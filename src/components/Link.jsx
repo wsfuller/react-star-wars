d68bee6918ddeef0d96ae48365b8dfde98536b22
wfuller <wfuller87@gmail.com>
2019-07-02 09:40:09 -0700

    Adding Link component
    Help to keep consistency with all links, do need to circle back and update Link to work with both external and internal links (React Router)

---
+++ src/components/Link.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  link: {
+    ...theme.link,
+    transition: 'all .5s ease-in-out',
+    '&:hover': {
+      color: theme.link.hoverColor,
+    },
+  },
+}));
+
+const Link = ({
+  href, target, text, external,
+}) => {
+  const classes = useStyles();
+
+  // TODO: Need to add logic here for Internal Links using React Router and external links using standard anchor tag
+
+  return (
+    <a href={href} className={classes.link} target={target}>
+      {text}
+    </a>
+  );
+};
+
+Link.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+  href: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  target: PropTypes.string,
+  external: PropTypes.bool,
+};
+
+Link.defaultProps = {
+  target: '_self',
+  external: false,
+};
+
+export default Link;
