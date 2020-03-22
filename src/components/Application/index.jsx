dbda2913deabccea75cd6f05bffe7b063bab824b
wfuller <wfuller87@gmail.com>
2020-03-22 16:51:00 -0700

    update

--- src/components/Application/index.jsx
+++ src/components/Application/index.jsx
@@ -18,6 +18,26 @@ const useStyles = makeStyles(() => ({
 const App = () => {
   const classes = useStyles();
 
+  async function supportsWebp() {
+    // eslint-disable-next-line no-restricted-globals
+    if (!self.createImageBitmap) {
+      console.log('noper nope nope');
+      return false;
+    }
+
+    const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+    const blob = await fetch(webpData).then(r => r.blob());
+    return createImageBitmap(blob).then(() => true, () => false);
+  }
+
+  (async () => {
+    if (await supportsWebp()) {
+      console.log('does support');
+    } else {
+      console.log('does not support');
+    }
+  })();
+
   return (
     <Fragment>
       <Navbar />
